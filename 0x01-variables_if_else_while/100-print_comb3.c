f20a566de166b258449f54c33a155be9997359a4
Dankaroum <dan.karoum5@gmail.com>
2023-10-06 18:15:51 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 9 ; i++)
+	{
+		j = i + 1;
+		do {
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(32);
+			}
+			i++;
+		} while (j < 10);
+	}
+	putchar('\n');
+	return (0);
+}
+
